583c13efbeb2913bc017960adad3843529216057
Cameron Woodard <cwoodard@bayviewglen.ca>
2023-03-07 11:21:05 -0500

    added enemy class

--- src/zork/zork/Room.java
+++ src/zork/zork/Room.java
@@ -6,6 +6,8 @@ public class Room {
 
   private String roomName;
   private String description;
+  private ArrayList<Item> groundItems;
+  private ArrayList<
   private ArrayList<Exit> exits;
 
   public ArrayList<Exit> getExits() {
