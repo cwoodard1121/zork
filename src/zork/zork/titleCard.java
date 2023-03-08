b35c6d02d16a075e8bd1e971cf2d5939992090f3
Lucca Minato <lminato@bayviewglen.ca>
2023-03-08 09:20:53 -0500

    Title card and opening credits

--- src/zork/zork/titleCard.java
+++ src/zork/zork/titleCard.java
@@ -19,7 +19,7 @@ public class titleCard {
         System.out.println("*   | ╚═╝     ╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝   ╚═╝    | *");
         System.out.println("  * |___________________________________________________________________________________|    *\n     .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *\n");
         System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n");
-        System.out.println("A text-based adventure video game created by: \n\n C.Woodard - rendering, gameplay, code \n\n E.McMullen - graphics, code \n\n M.Carnegie - gameplay, story, code \n\n and L.Minato - story, map layout, code \n");
+        System.out.println("A text-based adventure video game created by: \n\n C.Woodard \n\n E.McMullen \n\n M.Carnegie \n\n and L.Minato\n");
         System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
     }
 }
