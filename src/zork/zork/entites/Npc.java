6279cebd0d370672566d5991b5c26035ab697a69
MCarnegie <mcarnegie@bayviewglen.ca>
2023-03-08 09:14:36 -0500

    fixed npc stuff

--- src/zork/zork/entites/Npc.java
+++ src/zork/zork/entites/Npc.java
@@ -2,18 +2,20 @@ package zork.entites;
 
 import java.util.ArrayList;
 
+import datatypes.Location;
 import zork.Entity;
 import zork.Item;
+import zork.Room;
 
 public class Npc extends Entity {
-    private int x;
-    private int y;
+    private Location location;
+    private Room currentRoom;
     private int health;
     private ArrayList<Item> inventory;
     private String dialouge;
 
-    public Npc(int x, int y, int health, ArrayList<Item> inventory, String dialogue){
-        super(x, y, health, inventory);
+    public Npc(Location location,Room currentRoom, int health, ArrayList<Item> inventory, String dialogue){
+        super(location, currentRoom, health, inventory);
         this.dialouge = dialogue;
     }
 
