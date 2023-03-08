4e283cef1080013072735eeb52a08c5e14b77af9
Lucca Minato <lminato@bayviewglen.ca>
2023-03-08 08:53:44 -0500

    First iteration of york mills area

---
+++ src/zork/zork/storyBoard.java
@@ -0,0 +1,33 @@
+package zork;
+
+public class storyBoard {
+    /*Start in pickering in opening scene, get to york mills station (first area).
+    
+     * Read a sign that shows which busses go where.
+     * --North leads to bus terminal
+     * --South leads to subways (closed due to common TTC safety issue, soft locked)
+     * --west leads to faculty rest area
+     * --east leads back to go trains (soft locked)
+     * 
+     * (North of York Mills(terminal))
+     * --Yonge street shuttle bus
+     * ----First enemy is homeless guy taking up 3 seats. You die without a weapon. Displays "it would be useful to have something to fight with"
+     * 
+     * --York mills 95E bus
+     * 
+     * (South of York Mills(subway))
+     * --Note reading subway closed between x stations
+     * --potential easter egg
+     * 
+     * (West of York Mills(Faculty area))
+     * --Items
+     * ----Homeless man's leather cap (when equipped, fake stat "self - loathing" is increased by 1)
+     * ----Homeless man's old guitar (when pressed action, it falls apart)
+     * ----Weathered leather gloves (Starting weapon, low damage)
+     * 
+     * (East of York Mills(GO train tracks))
+     * --Option to buy a ticket back, game over if they buy the ticket and go back to pickering.
+     * --Option to jump down on track for a multitool that "could prove highly useful". If player goes down immediately, 
+     *   they get run over by train. If they come back later, they get the multitool. 
+     */
+}
