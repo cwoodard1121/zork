85905349ede987a8acd72d9ce9ed6237ed50576e
MCarnegie <mcarnegie@bayviewglen.ca>
2023-03-09 11:00:50 -0500

    started teh fights made a homeless guy and edited the enemy class

---
+++ src/zork/zork/enimes/HomelessGuy.java
@@ -0,0 +1,33 @@
+package zork.enimes;
+
+import java.util.ArrayList;
+
+import zork.entites.Enemy;
+import zork.items.Weapon;
+import datatypes.Location;
+import zork.Entity;
+import zork.Item;
+import zork.Moves;
+import zork.Room;
+
+public class HomelessGuy extends Enemy {
+
+    public HomelessGuy(Location location, Room currentRoom, int health, ArrayList<Item> inventory, ArrayList<Moves> moves, int money){
+        super(location, currentRoom, health, inventory, moves, money);
+    }
+
+    public void create() {
+       //needs location
+       //needs current room
+        setHealth(15);
+        Weapon glove = new Weapon(10, "dirty gloves", false, 2);
+        ArrayList<Item> stuff = new ArrayList<>(); stuff.add(glove);
+        setInventory(stuff);
+        Moves punch = new Moves("punch", 0, null);
+        ArrayList<Moves> attacks = new ArrayList<>(); attacks.add(punch);
+        setMoves(attacks);
+        setMoney(2.50);
+
+    }
+    
+}
--- src/zork/zork/entites/Enemy.java
+++ src/zork/zork/entites/Enemy.java
@@ -9,13 +9,18 @@ import zork.Moves;
 import zork.Room;
 
 public class Enemy extends Entity {
+    private Location location;
+    private Room currentRoom;
     private int health;
-    private ArrayList<Moves> moves;
     private ArrayList<Item> inventory;
+    private ArrayList<Moves> moves;
+    private double moneyDroped;
 
-    public Enemy(Location location, Room currentRoom, int health, ArrayList<Item> inventory, ArrayList<Moves> moves){
+
+    public Enemy(Location location, Room currentRoom, int health, ArrayList<Item> inventory, ArrayList<Moves> moves, int money){
         super(location, currentRoom, health, inventory);
         this.moves = moves;
+        this.moneyDroped = money;
     }
 
 
@@ -30,6 +35,49 @@ public class Enemy extends Entity {
 
     public ArrayList<Item> getInventory(){
         return inventory;
+    }
+
+
+    public Location getLocation() {
+        return location;
+    }
+
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+
+    public void setCurrentRoom(Room currentRoom) {
+        this.currentRoom = currentRoom;
+    }
+
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+
+    public void setInventory(ArrayList<Item> inventory) {
+        this.inventory = inventory;
+    }
+
+
+    public void setMoves(ArrayList<Moves> moves) {
+        this.moves = moves;
+    }
+
+    public double getMoney(){
+        return moneyDroped;
+    }
+
+    public void setMoney(double d){
+        this.moneyDroped = d;
     }
 
 }
