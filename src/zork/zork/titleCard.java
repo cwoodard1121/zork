e19a1b58e56b5a0375920702b7ea5a92ede15f40
EthanMcMullen <113043142+EthanMcMullen@users.noreply.github.com>
2023-03-09 12:18:44 -0500

    Slow text speed on title card!

--- src/zork/zork/titleCard.java
+++ src/zork/zork/titleCard.java
@@ -1,25 +1,47 @@
 package zork;
 
+import javax.swing.Renderer;
+
 public class titleCard {
-    public void printTitle() {
-        System.out.println("\n _ _ _ ___| |___ ___ _____ ___   | |_ ___       ");
-        System.out.println("| | | | -_| |  _| . |     | -_|  |  _| . |_ _ _ ");
-        System.out.println("|_____|___|_|___|___|_|_|_|___|  |_| |___|_|_|_|\n\n");    
-        System.out.println("     .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *");
-        System.out.println("    _____________________________________________________________________________________");
-        System.out.println("*   |                                                                                   | *");
-        System.out.println("  * | ██████╗ ██████╗ ██╗███╗   ███╗███████╗ ██████╗ ██╗   ██╗███████╗███████╗████████╗ |    *");
-        System.out.println("*   | ██╔══██╗██╔══██╗██║████╗ ████║██╔════╝██╔═══██╗██║   ██║██╔════╝██╔════╝╚══██╔══╝ | *");
-        System.out.println("  * | ██████╔╝██████╔╝██║██╔████╔██║█████╗  ██║   ██║██║   ██║█████╗  ███████╗   ██║    |    *");
-        System.out.println("*   | ██╔═══╝ ██╔══██╗██║██║╚██╔╝██║██╔══╝  ██║▄▄ ██║██║   ██║██╔══╝  ╚════██║   ██║    | *");
-        System.out.println("  * | ██║     ██║  ██║██║██║ ╚═╝ ██║███████╗╚██████╔╝╚██████╔╝███████╗███████║   ██║    |    *");
-        System.out.println("*   | ╚═╝     ╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝   ╚═╝    | *");
-        System.out.println("  * |___________________________________________________________________________________|    *\n     .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *\n");
-        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n");
-        System.out.println("A text-based adventure video game created by: \n\n C.Woodard \n\n E.McMullen \n\n M.Carnegie \n\n and L.Minato\n");
-        System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
-        System.out.println(" ___________________________________________________________________________________");
-        System.out.print(" Type Start to begin: ");
+    public void printTitle() throws InterruptedException {
+        Graphics g = new Graphics(null);
+        int delay = 4;
+        System.out.println();
+        g.slowTextSpeed(" _ _ _ ___| |___ ___ _____ ___   | |_ ___       ", delay);
+        g.slowTextSpeed("| | | | -_| |  _| . |     | -_|  |  _| . |_ _ _ ", delay);
+        g.slowTextSpeed("|_____|___|_|___|___|_|_|_|___|  |_| |___|_|_|_|", delay);
+        System.out.println("\n");
+        g.slowTextSpeed("     .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *", delay);
+        g.slowTextSpeed("    _____________________________________________________________________________________", delay);
+        g.slowTextSpeed("*   |                                                                                   | *", delay);
+        g.slowTextSpeed("  * | ██████╗ ██████╗ ██╗███╗   ███╗███████╗ ██████╗ ██╗   ██╗███████╗███████╗████████╗ |    *", delay);
+        g.slowTextSpeed("*   | ██╔══██╗██╔══██╗██║████╗ ████║██╔════╝██╔═══██╗██║   ██║██╔════╝██╔════╝╚══██╔══╝ | *", delay);
+        g.slowTextSpeed("  * | ██████╔╝██████╔╝██║██╔████╔██║█████╗  ██║   ██║██║   ██║█████╗  ███████╗   ██║    |    *", delay);
+        g.slowTextSpeed("*   | ██╔═══╝ ██╔══██╗██║██║╚██╔╝██║██╔══╝  ██║▄▄ ██║██║   ██║██╔══╝  ╚════██║   ██║    | *", delay);
+        g.slowTextSpeed("  * | ██║     ██║  ██║██║██║ ╚═╝ ██║███████╗╚██████╔╝╚██████╔╝███████╗███████║   ██║    |    *", delay);
+        g.slowTextSpeed("*   | ╚═╝     ╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝   ╚═╝    | *", delay);
+        g.slowTextSpeed("  * |___________________________________________________________________________________|    *", delay);
+        System.out.println();
+        g.slowTextSpeed("     .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *  .  *", delay);
+        System.out.println();
+        g.slowTextSpeed(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::", delay);
+        g.slowTextSpeed("A text-based adventure video game created by:", delay);
+        System.out.println();
+        delay = 50;
+        g.slowTextSpeed("C. Woodard", delay);
+        System.out.println("\n");
+        g.slowTextSpeed("E. McMullen", delay);
+        System.out.println("\n");
+        g.slowTextSpeed("M. Carnegie", delay);
+        System.out.println("\n");
+        g.slowTextSpeed("L. Minato", delay);
+        System.out.println("\n");
+        delay = 4;
+        g.slowTextSpeed(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::", delay);
+        g.slowTextSpeed("___________________________________________________________________________________", delay);
+        System.out.print("\nType Start to begin: ");
     }
+
+    // \n\n C.Woodard \n\n E.McMullen \n\n M.Carnegie \n\n and L.Minato\n"
 }
 
