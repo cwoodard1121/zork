cc3aa53e913365890a764088d2d15be18e0198cf
Cameron Woodard <cwoodard@bayviewglen.ca>
2023-03-09 11:41:45 -0500

    sound testing

--- src/zork/zork/Constants.java
+++ src/zork/zork/Constants.java
@@ -1,7 +1,21 @@
 package zork;
 
+import java.applet.AudioClip;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import datatypes.SoundPlayerUsingClip;
+import zork.commands.EnterSubway;
 import zork.commands.Go;
 import zork.commands.Stop;
 
@@ -19,5 +33,37 @@ public class Constants {
     public static void initCommands() {
         CommandConstants.commands.put("go", new Go("go"));
         CommandConstants.commands.put("stop", new Stop("stop"));
+        CommandConstants.commands.put("entersubway", new EnterSubway("entersubway"));
     }
+
+    public static synchronized void subwaySound() {
+        playSound("subway.wav");
+    }
+
+    public static synchronized void playSound(String soundName) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File soundFile = new File("C:\\Users\\cwoodard\\Desktop\\zork repo\\zork\\bin\\zork\\data\\" + soundName);
+                    System.out.println(soundFile.getPath());
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream("subway.wav"));
+                    AudioFormat audioFormat = inputStream.getFormat();
+                    DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+                    Clip clip = (Clip) AudioSystem.getLine(info);
+                    clip.addLineListener(new SoundPlayerUsingClip());
+                    clip.open(inputStream);
+                    clip.start();
+                    clip.close();
+                    inputStream.close();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        
+
+    }
+
 }
