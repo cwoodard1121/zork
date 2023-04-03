8a6faa22eb94b1a49a915e18b35b12ec253b6003
EthanMcMullen <113043142+EthanMcMullen@users.noreply.github.com>
2023-04-03 11:31:12 -0400

    Some commands

---
+++ src/zork/zork/commands/North.java
@@ -0,0 +1,25 @@
+package zork.commands;
+
+import zork.Command;
+import zork.Game;
+import zork.Exit;
+
+public class North extends Command {
+
+    public North(String name) {
+        super(name);
+    }
+
+    @Override
+    public String runCommand(String... args) {
+        for(Exit e : Game.getGame().getPlayer().getCurrentRoom().getExits()) {
+            if(e.getDirection().equalsIgnoreCase("north")) {
+                Game.getGame().getPlayer().changeRoom(e.getAdjacentRoom());
+                return e.getAdjacentRoom().getDescription();
+            }
+        }
+        return "There is no room to the North";
+
+
+    }
+}
---
+++ src/zork/zork/commands/West.java
@@ -0,0 +1,29 @@
+package zork.commands;
+
+import zork.Command;
+import zork.Constants;
+import zork.Exit;
+import zork.Game;
+import zork.Utils;
+
+public class West extends Command {
+    public West(String name) {
+        super(name);
+    }
+
+    @Override
+    public String runCommand(String... args) {
+        for (Exit e : Game.getGame().getPlayer().getCurrentRoom().getExits()) {
+            if (e.getDirection().equalsIgnoreCase("west")) {
+                Game.getGame().getPlayer().setCurrentRoom(e.getAdjacentRoom());
+                return e.getAdjacentRoom().getDescription();
+            }
+            
+        }
+        return "There is no room to the West";
+    }
+
+    
+    
+}
+
