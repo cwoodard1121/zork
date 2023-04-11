ca0ed1d58b00f47e94317a3ad076cf3c83bcc7c2
EthanMcMullen <113043142+EthanMcMullen@users.noreply.github.com>
2023-04-11 11:35:22 -0400

    sakfbsjdvbdvhbsdhf

--- src/zork/zork/Game.java
+++ src/zork/zork/Game.java
@@ -21,8 +21,8 @@ public class Game {
   private final Gson gson = new Gson();
   public static Game game = new Game();
   public static boolean finished = false;
-  public static boolean shouldCreateRooms = false;
-  public static boolean isTesting = false;
+  public static boolean shouldCreateRooms = true;
+  public static boolean isTesting = true;
   public static HashMap<String, Room> roomMap = new HashMap<String, Room>();
 
   private final Player player;
